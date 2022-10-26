a93310e2dd117cc9879b84219c071c56165e6830
Ghansham-GAP <ghanshampawar25@gmail.com>
2022-10-26 20:59:34 +0530

    Intiial commit

---
+++ Assignment - 5/Problem_5_1.c
@@ -0,0 +1,53 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Problem Statement : Write a program which accepts number from user
+//                     and display its multiplication.
+//
+// Input : 12
+// Output : 144
+//
+// Input : 13
+// Output : 1
+//
+// Author : Ghansham Ashok Pawar
+// Date : 25-10-2022
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+// Header file inclusion
+#include<stdio.h>
+
+
+// Function solution
+int MultFact(int iNo)
+{
+    int iAns = 1;
+
+    for(int i = 1; i <= iNo/2; i++)
+    {
+        if(iNo % i == 0)
+        {
+            iAns = iAns * i;
+        }
+    }
+
+    return iAns;   
+}
+
+
+// Entry point function
+int main()
+{
+    int iValue = 0, iRet = 0;   // Local variables
+
+    printf("Enter a number : \n");    
+    scanf("%d", &iValue);           // Accepting input
+
+    iRet = MultFact(iValue);    // function call
+
+    printf("-> %d\n", iRet);
+
+    return 0;    // Return 0
+}
